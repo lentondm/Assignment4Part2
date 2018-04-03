1170ddfeab771358a5eb08bec36000dee6f535f2
Daniel Lenton <lentondm@vcu.edu>
2018-04-03 10:25:29 -0400

    Add getters and setters to Contact class

--- app/src/main/java/edu/vcu/myapplication/Contact.java
+++ app/src/main/java/edu/vcu/myapplication/Contact.java
@@ -3,4 +3,17 @@ package edu.vcu.myapplication;
 public class Contact {
     String firstWord;
     String secondWord;
+
+    public String getFirst(){
+        return this.firstWord;
+    }
+    public String getSecond(){
+        return this.secondWord;
+    }
+    public void setFirst(String s){
+        this.firstWord = s;
+    }
+    public void setSecond(String s){
+        this.secondWord = s;
+    }
 }
