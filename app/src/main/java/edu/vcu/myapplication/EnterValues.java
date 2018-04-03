a73ff98c2ee441dc85e5a3c7a8a9d850655a91f6
Daniel Lenton <lentondm@vcu.edu>
2018-04-03 10:32:24 -0400

    Make submit button insert pair of words into database

--- app/src/main/java/edu/vcu/myapplication/EnterValues.java
+++ app/src/main/java/edu/vcu/myapplication/EnterValues.java
@@ -2,11 +2,26 @@ package edu.vcu.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class EnterValues extends Activity {
+
+    DatabaseHelper helper = new DatabaseHelper(this);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.enter_values);
+    }
+
+    public void onSubmitClick(View v){
+        EditText first = (EditText)findViewById(R.id.firstWord);
+        EditText second = (EditText)findViewById(R.id.secondWord);
+
+        Contact c = new Contact();
+        c.setFirst(first.toString());
+        c.setSecond(second.toString());
+        helper.insertContact(c);
     }
 }
