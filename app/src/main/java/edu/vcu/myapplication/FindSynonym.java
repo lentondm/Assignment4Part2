aa0a9d0887e41356086019236554d3b2a4095710
Daniel Lenton <lentondm@vcu.edu>
2018-04-03 10:47:32 -0400

    Find synonym searches database, displays result

--- app/src/main/java/edu/vcu/myapplication/FindSynonym.java
+++ app/src/main/java/edu/vcu/myapplication/FindSynonym.java
@@ -2,13 +2,27 @@ package edu.vcu.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class FindSynonym extends Activity {
+
+    DatabaseHelper helper = new DatabaseHelper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.enter_values);
         String word = getIntent().getStringExtra("word");
+        TextView tv = (TextView)findViewById(R.id.resultString);
+
+        String result = helper.searchWord(word);
+        if (word.equals(result))
+        {
+            tv.setText(helper.findSynonym(result));
+        }
+        else
+        {
+            tv.setText("Word not found");
+        }
     }
 }
