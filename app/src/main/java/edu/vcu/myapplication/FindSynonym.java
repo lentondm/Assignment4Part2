77aab09220c040c96018da7577368fd4cd984d3f
Daniel Lenton <lentondm@vcu.edu>
2018-04-03 08:44:33 -0400

    Create find synonym activity file and java file

---
+++ app/src/main/java/edu/vcu/myapplication/FindSynonym.java
@@ -0,0 +1,13 @@
+package edu.vcu.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FindSynonym extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.enter_values);
+    }
+}
