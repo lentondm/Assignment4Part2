516148987e9d3e658e7ac5260af06b8f1c00c524
Daniel Lenton <lentondm@vcu.edu>
2018-04-03 08:25:21 -0400

    Create enter values activity and java file

---
+++ app/src/main/java/edu/vcu/myapplication/EnterValues.java
@@ -0,0 +1,12 @@
+package edu.vcu.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EnterValues extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.enter_values);
+    }
+}
