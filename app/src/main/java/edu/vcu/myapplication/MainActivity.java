85c9b4b22940a53df8db5cfdfb57e5b9f1eb963e
Daniel Lenton <lentondm@vcu.edu>
2018-04-03 08:39:47 -0400

    Enter values button starts enter_values activity

---
+++ app/src/main/java/edu/vcu/myapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package edu.vcu.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onValuesButtonClick(View v){
+        if (v.getId() == R.id.valuesB) {
+            Intent i = new Intent(MainActivity.this, EnterValues.class);
+            startActivity(i);
+        }
+    }
+}
